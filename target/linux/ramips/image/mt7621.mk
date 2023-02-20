e4c3c2ea30c529ac0d5839c6be5af76431143043
vvvvsp <125775745+vvvvsp@users.noreply.github.com>
2023-02-20 13:34:51 +0800

    Update mt7621.mk

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1350,7 +1350,7 @@ endef
 TARGET_DEVICES += oraybox_x3a
 
 define Device/phicomm_k2p
-  IMAGE_SIZE := 15744k
+  IMAGE_SIZE := 32448k
   DEVICE_VENDOR := Phicomm
   DEVICE_MODEL := K2P
   SUPPORTED_DEVICES += k2p
